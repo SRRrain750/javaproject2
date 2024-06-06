a5e947b4b87fc87d044f4642ce01301dcede2bed
SRRrain750 <160513264+SRRrain750@users.noreply.github.com>
2024-06-06 18:16:24 +0530

    Add files via upload

---
+++ MakeFile.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+class Makeile{
+
+public static void main(String[]args)throws IOException{
+
+
+
+FileOutputStream f=new FileOutputStream("myfile.txt");
+
+f.write('2');
+
+
+
+
+
+
+
+f.close();
+}
+}
